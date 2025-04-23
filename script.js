b5b9ea2ab490bbcb28d234ba294e5158050d772b
Davi Calasans <davivida878@gmail.com>
2025-04-22 21:39:15 -0300

    refactor: Simplificar a criação de elementos por remover funções desnecessárias

--- script.js
+++ script.js
@@ -4,36 +4,20 @@ const elements = [
     {tag: 'footer', text: 'Frase 3'},
     {tag: 'section', text: 'Frase 4'}
 ];
-//Desestrutura cada objeto contido no array através do loop for e chama a função addElement() passando tag e text como argumentos.
-function desestructionTag(elements){
-    for(i = 0; i <= elements.length - 1; i++){
-        const {tag, text} = elements[i]; 
-        addElement(tag, text);
 
-    }
-};
+const container = document.querySelector('.container');
+const div = document.createElement('div');
 
-//Cria a div que vai conter o elementos criados em addElement().
-function createDivFor(){
-    const container = document.querySelector('.container')
-    const divFor = document.createElement('div');
-    divFor.className = 'div-for';
-    container.appendChild(divFor);
+for(let i = 0; i < elements.length; i++){
+    let { tag, text} = elements[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = text;
+    div.appendChild(tagCriada);
 }
 
-//Cria um novo elemento com tag e text como parametros de tagName e conteúdo da tag, respectivamente.
-function addElement(tag, text){
-    const divFor = document.querySelector('.div-for');
-    const newElement = document.createElement(tag);
-    newElement.innerHTML = text;
-    divFor.appendChild(newElement);
-
-    
-}
+container.appendChild(div);
 
 
-createDivFor();
-desestructionTag(elements);
 
 /*Criar elementos HTML a partir da desestruturação do array que contém a tag HTML referente ao elemento e o text que deverá aparecer em cada elemento criado, respectivamente. Além disso, criar um elemento maior que contenha os elementos que serão criados via desestruturação */
 
